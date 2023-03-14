5a13064ad113f842c8c3b94dd10a852f02d8ccfc
himedsoso <himed.abidar@gmail.com>
2023-03-15 00:36:36 +0100

    first commit

--- src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -1,6 +1,12 @@
 package com.wildcodeschool.wildandwizard.controller;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SchoolController {
-
+    @Autowired
+    private SchoolRepository repository;
     // TODO : get school repository by dependency injection
 
     @GetMapping("/schools")
     public String getAll(Model model) {
-
+        model.addAttribute("schools", repository.findAll());
         // TODO : find all schools
 
         return "schools";
@@ -24,7 +31,15 @@ public class SchoolController {
     @GetMapping("/school")
     public String getSchool(Model model,
                             @RequestParam(required = false) Long id) {
-
+                                School school = new School();
+                                if (id != null) {
+                                    Optional<School> optionalSchool = repository.findById(id);
+                                    if (optionalSchool.isPresent()) {
+                                        school = optionalSchool.get();
+                                    }
+                                }
+                                model.addAttribute("school", school);
+                        
         // TODO : find one school by id
 
         return "school";
@@ -32,7 +47,7 @@ public class SchoolController {
 
     @PostMapping("/school")
     public String postSchool(@ModelAttribute School school) {
-
+        repository.save(school);
         // TODO : create or update a school
 
         return "redirect:/schools";
@@ -40,9 +55,15 @@ public class SchoolController {
 
     @GetMapping("/school/delete")
     public String deleteSchool(@RequestParam Long id) {
-
+        repository.deleteById(id);
         // TODO : delete a school
 
         return "redirect:/schools";
     }
+
+   
+   
+
+
+    
 }
